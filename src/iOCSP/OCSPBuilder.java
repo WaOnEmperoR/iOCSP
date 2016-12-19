cdb7f2d518761649da5d959e12f4bf368ec26afb
WaOnEmperoR <themusketeer17@gmail.com>
2016-12-19 08:55:38 +0700

    Successfully construct Hash of Issuer Name and Hash of Public Key as ASN.1 Octet String

--- src/iOCSP/OCSPBuilder.java
+++ src/iOCSP/OCSPBuilder.java
@@ -14,12 +14,14 @@ import java.util.Calendar;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.ocsp.CertID;
@@ -51,16 +53,11 @@ public class OCSPBuilder {
         AlgorithmIdentifier alg_id = new AlgorithmIdentifier(myObjAlg);
 
         try {
-            
-            
-//            ASN1InputStream stream_issuer_name = new ASN1InputStream(issuer_name);
-//            ASN1InputStream stream_public_key = new ASN1InputStream(pub_key);
-           
             org.jasn.DerEncoder de_issuer_name = new org.jasn.DerEncoder();
             org.jasn.DerEncoder de_public_key = new org.jasn.DerEncoder();
             
-            de_issuer_name.encodeBitString(issuer_name);
-            de_public_key.encodeBitString(pub_key);
+            de_issuer_name.encodeOctetString(issuer_name);
+            de_public_key.encodeOctetString(pub_key);
             
             ByteArrayOutputStream baos_1 = new ByteArrayOutputStream();
             de_issuer_name.writeTo(baos_1);
@@ -73,16 +70,23 @@ public class OCSPBuilder {
             ASN1InputStream stream_issuer_name = new ASN1InputStream(der_byte_issuer_name);
             ASN1InputStream stream_public_key = new ASN1InputStream(der_byte_public_key);
             
-            ASN1OctetString octs_issuer_name = (ASN1OctetString) stream_issuer_name.readObject();
-            ASN1OctetString octs_public_key = (ASN1OctetString) stream_public_key.readObject();
+            ASN1Encodable asn1_issuer_name = (ASN1Encodable) stream_issuer_name.readObject();
+            ASN1Encodable asn1_public_key = (ASN1Encodable) stream_public_key.readObject();
             
+            ASN1Primitive prim_issuer_name = asn1_issuer_name.toASN1Primitive();
+            ASN1Primitive prim_public_key = asn1_public_key.toASN1Primitive();
+            
+            ASN1OctetString octs_issuer_name = (ASN1OctetString) prim_issuer_name;
+            ASN1OctetString octs_public_key = (ASN1OctetString) prim_public_key;
+
             CertID c1 = new CertID(alg_id, octs_issuer_name, octs_public_key, ASN1_SN);
             byte[] hasil = (c1.toASN1Primitive().getEncoded("DER"));
 
             Request r1 = new Request(c1, null);
             
             UUID uuid1 = UUID.randomUUID();
-
+            System.out.println(uuid1);
+            
             try (FileOutputStream fileOuputStream = new FileOutputStream("Request_" + uuid1 + ".DER")) {
                 fileOuputStream.write(r1.getEncoded("DER"));
             }
